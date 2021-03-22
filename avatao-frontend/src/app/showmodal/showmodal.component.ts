11a3172b75dd18a0273a9aa8514c6bff20dd354a
Dobi Tamas <dobi.tamas0428@gmail.com>
2021-03-22 13:12:51 +0100

    Added showmodal component to show the two modals

---
+++ avatao-frontend/src/app/showmodal/showmodal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-showmodal',
+  templateUrl: './showmodal.component.html',
+  styleUrls: ['./showmodal.component.css']
+})
+export class ShowmodalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
