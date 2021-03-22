5911a0db7427dcfa0141224a0f0768f2debdd666
Dobi Tamas <dobi.tamas0428@gmail.com>
2021-03-22 13:16:50 +0100

    Created function to open modals

--- avatao-frontend/src/app/showmodal/showmodal.component.ts
+++ avatao-frontend/src/app/showmodal/showmodal.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowmodalComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  onCreateModal(messageType:'PlatformReview' | 'TaskReview') {
+    
   }
 
 }
