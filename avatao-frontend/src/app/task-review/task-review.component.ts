6062de8c593e3a94aa05613795e56df7d97d7cec
Dobi Tamas <dobi.tamas0428@gmail.com>
2021-03-23 09:50:49 +0100

    Added select buttons

--- avatao-frontend/src/app/task-review/task-review.component.ts
+++ avatao-frontend/src/app/task-review/task-review.component.ts
@@ -1,15 +1,75 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Feedback_Button {
+  text: string,
+  status: boolean,
+  score : number
+}
+
+interface Feedback_Choice {
+  input?: string,
+  button?: Feedback_Button
+}
+
 @Component({
   selector: 'app-task-review',
   templateUrl: './task-review.component.html',
   styleUrls: ['./task-review.component.css']
 })
 export class TaskReviewComponent implements OnInit {
+  structureButtons?: Feedback_Button[];
 
-  constructor() { }
+  confidenceButtons?: Feedback_Button[];  
+
+
+  constructor() {
+   }
 
   ngOnInit(): void {
+    this.seedButtons();
+  }
+
+
+  seedButtons(): void {
+    this.structureButtons = [
+      {
+        text: "Confusing",
+        status: false,
+        score: 1
+      }, {
+        text: "Easy-to-follow",
+        status: false,
+        score: 2
+      }, {
+        text: "Logical",
+        status: false,
+        score: 3
+      }
+    ];
+
+    this.confidenceButtons = [
+      {
+        text:"Not at all",
+        status: false,
+        score: 1
+      },
+      {
+        text: "Not sure",
+        status: false,
+        score: 2
+      },
+      {
+        text: "Probably okay",
+        status: true,
+        score: 3
+      },
+      {
+        text: "Full confidence",
+        status: false,
+        score: 4
+      }
+    ];
+
   }
 
 }
